f130d57678fd8a3db83af799ba04f1189515f34b
Hannes Rosenkranz <Frodo@dynip-129-217-113-172.wifi.tu-dortmund.de>
2017-05-16 16:01:09 +0200

    added the SWT project

---
+++ SWT/SL/Swt-Student/src/exercise/hud/HudScore.java
@@ -0,0 +1,23 @@
+package exercise.hud;
+
+import edu.udo.cs.swtsf.core.Game;
+import edu.udo.cs.swtsf.core.player.Player;
+import edu.udo.cs.swtsf.core.player.PlayerObserver;
+import edu.udo.cs.swtsf.view.HudElement;
+import edu.udo.cs.swtsf.view.HudElementOrientation;
+import edu.udo.cs.swtsf.view.ViewManager;
+
+public class HudScore extends HudElement implements PlayerObserver {
+
+	public HudScore() {
+		super(HudElementOrientation.TOP, "HUD/Score", 0, 0, 32, 32);
+	}
+	
+	public void onScoreChanged(Player player) {
+		setText(Integer.toString(player.getScore()));
+	}
+	
+	protected void afterAdded(ViewManager view, Game game) {
+		onScoreChanged(game.getPlayer());
+	}
+}
